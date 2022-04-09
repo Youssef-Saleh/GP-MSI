d72d1fa0b66c15b7d89bccabb510919dc612f337
YoussefSaleh <youssef.s.saleh@Gmail.com>
2022-04-10 01:59:38 +0200

    modified model files to include significant proteins and edited results dataframe columns

--- Breast Cancer/samr_breast.R
+++ Breast Cancer/samr_breast.R
@@ -5,7 +5,7 @@ library(samr)
 
 
 
-setwd("C:/Users/omars/Desktop")
+setwd("C:/Users/omars/Desktop/GP-MSI/Breast Cancer")
 joindata <- read.csv("BreastCancerAverageSpectrumValues.csv")
 msdata <- joindata[,1:62]
 
@@ -15,7 +15,7 @@ samr.obj=samr(d, resp.type="Two class unpaired", nperms=200)
 # samr.obj=samr(d, resp.type="Multiclass", nperms=200)
 delta.table <- samr.compute.delta.table(samr.obj)
 
-delta=  1.6726661056                #(delta.table) this is the value in delta.table(FDR <0.001)
+delta=  1.0564811363                #(delta.table) this is the value in delta.table(FDR <0.001)
 
 samr.plot(samr.obj,delta)
 siggenes.table<-samr.compute.siggenes.table(samr.obj,delta, d, delta.table)
@@ -25,5 +25,5 @@ library(jsonlite)
 significant_proteins <- subset(siggenes.table$genes.lo , select=c("Gene ID"))
 write_json(significant_proteins, "significant_breast_proteins.json")
 
-#significant_proteins_up <- subset(siggenes.table$genes.up , select=c("Gene ID"))
-#write_json(significant_proteins_up, "significant_breast_proteins_up.json")
+significant_proteins_up <- subset(siggenes.table$genes.up , select=c("Gene ID"))
+write_json(significant_proteins_up, "significant_breast_proteins_up.json")
--- Gastric Cancer/samr_gastric.R
+++ Gastric Cancer/samr_gastric.R
@@ -5,7 +5,7 @@ library(samr)
 
 
 
-setwd("C:/Users/omars/Desktop")
+setwd("C:/Users/omars/Desktop/GP-MSI/Gastric Cancer")
 joindata <- read.csv("GastricCancerAverageSpectrumValues.csv")
 msdata <- joindata[,1:82]
 
@@ -16,7 +16,7 @@ samr.obj=samr(d, resp.type="Two class unpaired", nperms=200)
 # samr.obj=samr(d, resp.type="Multiclass", nperms=200)
 delta.table <- samr.compute.delta.table(samr.obj)
 
-delta=   1.356333898                             #(delta.table) this is the value in delta.table(FDR <0.001)
+delta=   1.049893013                             #(delta.table) this is the value in delta.table(FDR <0.001)
 
 samr.plot(samr.obj,delta)
 siggenes.table<-samr.compute.siggenes.table(samr.obj,delta, d, delta.table)
@@ -26,5 +26,5 @@ library(jsonlite)
 significant_proteins <- subset(siggenes.table$genes.lo , select=c("Gene ID"))
 write_json(significant_proteins, "significant_gastric_proteins.json")
 
-#significant_proteins_up <- subset(siggenes.table$genes.up , select=c("Gene ID"))
-#write_json(significant_proteins_up, "significant_gastric_proteins_up.json")
+significant_proteins_up <- subset(siggenes.table$genes.up , select=c("Gene ID"))
+write_json(significant_proteins_up, "significant_gastric_proteins_up.json")
