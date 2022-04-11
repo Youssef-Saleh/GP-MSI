4ef3b83bbf5c2cbba023de15bf4253ce63753a75
YoussefSaleh <youssef.s.saleh@Gmail.com>
2022-04-11 22:56:06 +0200

    Added comments to each cell in Gastric Model

--- Gastric Cancer/samr_gastric.R
+++ Gastric Cancer/samr_gastric.R
@@ -16,7 +16,7 @@ samr.obj=samr(d, resp.type="Two class unpaired", nperms=200)
 # samr.obj=samr(d, resp.type="Multiclass", nperms=200)
 delta.table <- samr.compute.delta.table(samr.obj)
 
-delta=   1.049893013                             #(delta.table) this is the value in delta.table(FDR <0.001)
+delta=   0.3547958431                             #(delta.table) this is the value in delta.table(FDR <0.001)
 
 samr.plot(samr.obj,delta)
 siggenes.table<-samr.compute.siggenes.table(samr.obj,delta, d, delta.table)
