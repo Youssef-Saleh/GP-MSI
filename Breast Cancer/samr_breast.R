3d9a7c6b3b2e5cfc75a86fc3ce26186e30727130
YoussefSaleh <youssef.s.saleh@Gmail.com>
2022-04-12 00:49:29 +0200

    Fixed writing file error in R studio, now creates empty list when no significant proteins are found

--- Breast Cancer/samr_breast.R
+++ Breast Cancer/samr_breast.R
@@ -1,10 +1,13 @@
-library(BiocManager)
-#BiocManager::install("impute",force = TRUE)
-library(samr)
+# Reading Imports
+{
+  library(BiocManager)
+  #BiocManager::install("impute",force = TRUE)
+  library(samr)
+}
 
 
-
-
+# Running SAMR Algorithm and creating delta table
+{
 setwd("E:/GP/MSI Dataset/Breast Cancer")
 joindata <- read.csv("BreastCancerAverageSpectrumValues.csv")
 msdata <- joindata[,1:62]
@@ -14,16 +17,48 @@ set.seed(3)
 samr.obj=samr(d, resp.type="Two class unpaired", nperms=200)
 # samr.obj=samr(d, resp.type="Multiclass", nperms=200)
 delta.table <- samr.compute.delta.table(samr.obj)
+}
 
-delta=  	1.572459934               #(delta.table) this is the value in delta.table(FDR <0.001)
 
-samr.plot(samr.obj,delta)
-siggenes.table<-samr.compute.siggenes.table(samr.obj,delta, d, delta.table)
-siggenes.table
+delta=   1.801753608   #(delta.table) this is the value in delta.table(FDR <0.001)
 
-library(jsonlite)
-significant_proteins <- subset(siggenes.table$genes.lo , select=c("Gene ID"))
-write_json(significant_proteins, "significant_breast_proteins.json")
 
-significant_proteins_up <- subset(siggenes.table$genes.up , select=c("Gene ID"))
-write_json(significant_proteins_up, "significant_breast_proteins_up.json")
+# Plot the significant proteins
+{
+  samr.plot(samr.obj,delta)
+  siggenes.table<-samr.compute.siggenes.table(samr.obj,delta, d, delta.table)
+  siggenes.table
+}
+
+# Create two files for the significant proteins to be read in the model code
+{
+  library(jsonlite)
+  
+  Significant_Proteins_LO <- tryCatch(
+    {
+      significant_proteins <- subset(siggenes.table$genes.lo , select=c("Gene ID"))
+      write_json(significant_proteins, "significant_gastric_proteins.json")
+    },
+    error = function(e){
+      print(e)
+      significant_proteins <- list(list())
+      write_json(significant_proteins, "significant_gastric_proteins.json")
+    }
+  )
+  Significant_Proteins_LO
+  
+  
+  Significant_Proteins_HI <- tryCatch(
+    {
+      significant_proteins_up <- subset(siggenes.table$genes.up , select=c("Gene ID"))
+      write_json(significant_proteins_up, "significant_gastric_proteins_up.json")
+    },
+    error = function(e){
+      print(e)
+      significant_proteins_up <- list(list())
+      write_json(significant_proteins_up, "significant_gastric_proteins.json")
+    }
+  )
+  
+  Significant_Proteins_HI
+}
--- Gastric Cancer/samr_gastric.R
+++ Gastric Cancer/samr_gastric.R
@@ -1,10 +1,13 @@
+# Reading Imports
+{
 library(BiocManager)
 #BiocManager::install("impute",force = TRUE)
 library(samr)
+}
 
 
-
-
+# Running SAMR Algorithm and creating delta table
+{
 setwd("C:/Users/omars/Desktop/GP-MSI/Gastric Cancer")
 joindata <- read.csv("GastricCancerAverageSpectrumValues.csv")
 msdata <- joindata[,1:82]
@@ -15,16 +18,48 @@ set.seed(3)
 samr.obj=samr(d, resp.type="Two class unpaired", nperms=200)
 # samr.obj=samr(d, resp.type="Multiclass", nperms=200)
 delta.table <- samr.compute.delta.table(samr.obj)
+}
 
-delta=   0.3547958431                             #(delta.table) this is the value in delta.table(FDR <0.001)
+delta=   1.801753608   #(delta.table) this is the value in delta.table(FDR <0.001)
 
+
+# Plot the significant proteins
+{
 samr.plot(samr.obj,delta)
 siggenes.table<-samr.compute.siggenes.table(samr.obj,delta, d, delta.table)
 siggenes.table
+}
 
+# Create two files for the significant proteins to be read in the model code
+{
 library(jsonlite)
-significant_proteins <- subset(siggenes.table$genes.lo , select=c("Gene ID"))
-write_json(significant_proteins, "significant_gastric_proteins.json")
 
-significant_proteins_up <- subset(siggenes.table$genes.up , select=c("Gene ID"))
-write_json(significant_proteins_up, "significant_gastric_proteins_up.json")
+Significant_Proteins_LO <- tryCatch(
+  {
+    significant_proteins <- subset(siggenes.table$genes.lo , select=c("Gene ID"))
+    write_json(significant_proteins, "significant_gastric_proteins.json")
+  },
+  error = function(e){
+    print(e)
+    significant_proteins <- list(list())
+    write_json(significant_proteins, "significant_gastric_proteins.json")
+  }
+)
+Significant_Proteins_LO
+
+
+Significant_Proteins_HI <- tryCatch(
+  {
+    significant_proteins_up <- subset(siggenes.table$genes.up , select=c("Gene ID"))
+    write_json(significant_proteins_up, "significant_gastric_proteins_up.json")
+  },
+  error = function(e){
+    print(e)
+    significant_proteins_up <- list(list())
+    write_json(significant_proteins_up, "significant_gastric_proteins.json")
+  }
+)
+
+Significant_Proteins_HI
+}
+
